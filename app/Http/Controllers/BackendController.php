1d26379ceaa552d573558f57c58006b3399a2393
shimulshaikh <shimulshaikhcse@gmail.com>
2020-11-16 10:36:35 +0600

    update highchart

--- app/Http/Controllers/BackendController.php
+++ app/Http/Controllers/BackendController.php
@@ -18,12 +18,21 @@ class BackendController extends Controller
     
     public function index()
     {
-    	$postData = Post::select(\DB::raw("COUNT(*) as count"))
+    	$post = Post::select(\DB::raw("COUNT(*) as count"))
                     ->whereYear('created_at', date('Y'))
                     ->groupBy(\DB::raw("Month(created_at)"))
                     ->pluck('count');
 
-                    //dd($postData);
+        $months = Post::select(\DB::raw("Month(created_at) as month"))
+                    ->whereYear('created_at', date('Y'))
+                    ->groupBy(\DB::raw("Month(created_at)"))
+                    ->pluck('month');    
+
+        $postData = array(0,0,0,0,0,0,0,0,0,0,0,0);                    
+
+        foreach ($months as $index => $month) {
+                    $postData[$month] = $post[$index];
+                }        
 
      	return view('website.backend.dashboard.index', compact('postData'));
     }
